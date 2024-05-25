27e27f22feb87df47f1dde74799bad7fc48ec340
Catalin Pirvu <catapirvu98@gmail.com>
2024-05-25 10:28:04 +0300

    Creating the GUI Window for the Application

---
+++ File Transfer.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import socket
+from tkinter import filedialog
+import os
+
+root = Tk()
+root.title("Share it")
+root.geometry("450x560+500+200")
+root.configure(bg="#f4fdfe")
+root.resizable(False,False)
+
+
+root.mainloop()
