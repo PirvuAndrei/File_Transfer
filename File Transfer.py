fec3e1bf650c237272d658d678ce52f909f8283f
Catalin Pirvu <catapirvu98@gmail.com>
2024-05-25 11:05:00 +0300

    Putting Widgets for Sending/Receiving inside Root

--- File Transfer.py
+++ File Transfer.py
@@ -10,4 +10,30 @@ root.configure(bg="#f4fdfe")
 root.resizable(False,False)
 
 
+#icon
+image_icon = PhotoImage(file="fileimages/icon.png")
+root.iconphoto(False, image_icon)
+
+Label(root, text="File Transfer",font=("Acumin Variable Concept", 20,'bold'),bg="#f4fdfe").place(x=20,y=30)
+
+Frame(root, width=400,height=2,bg="#f3f5f6").place(x=25,y=80)
+
+send_image = PhotoImage(file="fileimages/send.png")
+send = Button(root, image=send_image, bg="#f4fdfe",bd=0)
+send.place(x=50, y=100)
+
+receive_image = PhotoImage(file="fileimages/receive.png")
+receive = Button(root, image=receive_image, bg="#f4fdfe",bd=0)
+receive.place(x=300, y=100)
+
+#Label
+
+Label(root, text="Send", font=("Acumin Variable Concept", 17, 'bold'), bg="#f4fdfe").place(x=65, y=200)
+Label(root, text="Receive", font=("Acumin Variable Concept", 17, 'bold'), bg="#f4fdfe").place(x=300, y=200)
+
+
+background = PhotoImage(file="fileimages/background.png")
+Label(root,image=background).place(x=-2,y=323)
+
+
 root.mainloop()
